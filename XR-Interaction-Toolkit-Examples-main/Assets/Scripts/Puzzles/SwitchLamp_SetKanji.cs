b5e26b32a039b5385ab829a64025ba15f512ea68
Emilie Gonin <emilie.yuu@gmail.com>
2024-03-12 16:03:38 +0100

    add lamp clue shader

---
+++ XR-Interaction-Toolkit-Examples-main/Assets/Scripts/Puzzles/SwitchLamp_SetKanji.cs
@@ -0,0 +1,27 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class SwitchLamp_SetKanji : MonoBehaviour
+{
+    [SerializeField] int _id;
+    [SerializeField] TMP_Text _kanji;
+
+    private string[] _kanjiList;
+
+    private void Awake()
+    {
+        _kanjiList = new string[5] { "月", "火", "水", "木", "金" };
+        SetKanji();
+    }
+
+    public void SetKanjiId(int id)
+    {
+        _id = id;
+        SetKanji();
+    }
+
+    private void SetKanji()
+    {
+        _kanji.text = _kanjiList[_id];
+    }
+}
