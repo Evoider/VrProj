a1dd459d235644a3b4f198350654f856c73018fe
Emilie Gonin <emilie.yuu@gmail.com>
2024-03-14 14:26:09 +0100

    remove fake players + disable key effect on grab

--- XR-Interaction-Toolkit-Examples-main/Assets/Scripts/GrabKey.cs
+++ XR-Interaction-Toolkit-Examples-main/Assets/Scripts/GrabKey.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class GrabKey : MonoBehaviour
 {
     [SerializeField] private Rigidbody _rb;
+    [SerializeField] private VisualEffect _effect;
 
     public void ActivateGravity()
     {
         _rb.useGravity = true;
+        _effect.enabled = false;
     }
 }
