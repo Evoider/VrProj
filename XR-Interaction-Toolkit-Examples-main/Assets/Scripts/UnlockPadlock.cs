4bb5fc8d5679b7c991849dbe64f9238a750dd484
Yoann Rocher <117647468+Evoider@users.noreply.github.com>
2024-03-14 18:02:29 +0100

    Tentative Son

--- XR-Interaction-Toolkit-Examples-main/Assets/Scripts/UnlockPadlock.cs
+++ XR-Interaction-Toolkit-Examples-main/Assets/Scripts/UnlockPadlock.cs
@@ -5,12 +5,10 @@ using UnityEngine.XR.Interaction.Toolkit;
 public class UnlockPadlock : MonoBehaviour
 {
     
-    [SerializeField] private AudioSource _clic;
     public static event Action OnUnlock;
 
     public void Unlock(SelectEnterEventArgs args)
     {
-        _clic.Play();
         OnUnlock?.Invoke();
         Destroy(args.interactableObject.transform.gameObject);
     }
