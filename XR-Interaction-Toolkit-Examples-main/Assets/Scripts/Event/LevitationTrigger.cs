7a4311b5d37eac6dd7205614a1e81b95b3a3aa18
Aurelien Russier <97309733+Malkael@users.noreply.github.com>
2024-03-14 23:32:36 +0100

    Last Sound Design

--- XR-Interaction-Toolkit-Examples-main/Assets/Scripts/Event/LevitationTrigger.cs
+++ XR-Interaction-Toolkit-Examples-main/Assets/Scripts/Event/LevitationTrigger.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 public class LevitationTrigger : MonoBehaviour
 {
     public static event Action OnLevitated;
+    [SerializeField] private AudioSource _tableMoving;
 
     private void OnTriggerEnter(Collider other)
     {
+        _tableMoving.Play();
         OnLevitated?.Invoke();
+
     }
 }
